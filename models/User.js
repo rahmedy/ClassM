da395f8d39b3465ddf80fa97e366c0c45ab58119
Matt Stalzer <mjs58181@uga.edu>
2020-11-05 14:01:01 -0500

    creating new branch

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = User = mongoose.model("users", UserSchema);
--- models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    Profressor: require("./professorModel"),
-    Student: require("./studentModel")
-  };
