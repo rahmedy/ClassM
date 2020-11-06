d75af9c0c1edb9a30f3b57614c91f54c96b1f59e
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-11-06 01:01:35 -0500

    Created a login page and set up basic form for user to sign in

---
+++ client/src/views/Sign.js
@@ -0,0 +1,25 @@
+// import React from "react";
+
+// import {  Form, FormGroup, Label, Input,  } from 'reactstrap';
+
+// class Sign extends React.Component {
+
+//     render(){
+//         return(
+//           <>
+//     <Form>
+//       <FormGroup>
+//         <Label for="exampleEmail">Email</Label>
+//         <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+//       </FormGroup>
+//       <FormGroup>
+//         <Label for="examplePassword">Password</Label>
+//         <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+//       </FormGroup>
+//       </Form>
+//           </>  
+//         )
+//     }
+// }
+
+// export default Sign;
