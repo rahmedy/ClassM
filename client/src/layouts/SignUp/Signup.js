fa0be0eaf8dffda2faa013e06841729af007ba43
Rayan Ahmedy <rayanahmedy1999@gmail.com>
2020-11-07 00:35:23 -0500

    created Sighnup page, added sign up form under component , creadted signup navbar using bootstrap

--- client/src/layouts/SignUp/Signup.js
+++ client/src/layouts/SignUp/Signup.js
@@ -1,3 +1,22 @@
 import React from "react";
 import {  Switch ,Redirect} from "react-router-dom";
 
+import SignNav from "components/Navbars/SignNavbar.js"
+import SignP from "components/SignP/SignP.js"
+
+class Signup extends React.Component {
+    render() {
+        return (
+          <>
+            <div className="wrapper">
+                <SignNav />
+                <SignP />
+              
+            </div>
+          
+          </>
+        );
+      }
+}
+
+export default Signup;
