fd26cb38fb127ec0a020102bb7c3e3ce612e5a81
Matt Stalzer <mjs58181@uga.edu>
2020-11-10 21:44:35 -0500

    pulling from main

---
+++ models/Classes.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+    className: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports = User = mongoose.model("courses", UserSchema);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
+const courses = require("./routes/api/classes");
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -28,6 +29,8 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+app.use("/api/course", courses);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
