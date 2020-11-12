2197fc71cf412f1a8fc43257c6d6d4189af68b5d
imhaileynicole <66336162+imhaileynicole@users.noreply.github.com>
2020-11-12 18:00:29 -0500

    Update TableList.js

--- client/src/views/TableList.js
+++ client/src/views/TableList.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 
 // reactstrap components
@@ -37,124 +20,102 @@ class Tables extends React.Component {
             <Col md="12">
               <Card>
                 <CardHeader>
-                  <CardTitle tag="h4">Simple Table</CardTitle>
+                  <CardTitle tag="h4">Grade Scale(BASED ON A TOTAL OF 1500 POINTS) </CardTitle>
                 </CardHeader>
                 <CardBody>
                   <Table className="tablesorter" responsive>
                     <thead className="text-primary">
                       <tr>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th>City</th>
-                        <th className="text-center">Salary</th>
+                        <th>Letter Grade</th>
+                        <th>Grade Percentage</th>
+                        <th>Points</th>
+                        
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td>Dakota Rice</td>
-                        <td>Niger</td>
-                        <td>Oud-Turnhout</td>
-                        <td className="text-center">$36,738</td>
+                        <td>	A+</td>
+                        <td>97%</td>
+                        <td>1455 – 1500</td>
+                        
                       </tr>
                       <tr>
-                        <td>Minerva Hooper</td>
-                        <td>Curaçao</td>
-                        <td>Sinaai-Waas</td>
-                        <td className="text-center">$23,789</td>
+                        <td>A</td>
+                        <td>93 – 96.9%</td>
+                        <td>1395 – 1454</td>
+                        
                       </tr>
                       <tr>
-                        <td>Sage Rodriguez</td>
-                        <td>Netherlands</td>
-                        <td>Baileux</td>
-                        <td className="text-center">$56,142</td>
+                        <td>A-</td>
+                        <td>90 – 92.9%</td>
+                        <td>1350 – 1394</td>
+                        
                       </tr>
                       <tr>
-                        <td>Philip Chaney</td>
-                        <td>Korea, South</td>
-                        <td>Overland Park</td>
-                        <td className="text-center">$38,735</td>
+                        <td>B+</td>
+                        <td>87 – 89.9%</td>
+                        <td>1305 – 1349</td>
+                        
                       </tr>
                       <tr>
-                        <td>Doris Greene</td>
-                        <td>Malawi</td>
-                        <td>Feldkirchen in Kärnten</td>
-                        <td className="text-center">$63,542</td>
+                        <td>B</td>
+                        <td>83 – 86.9%</td>
+                        <td>1245 – 1304</td>
+        
                       </tr>
                       <tr>
-                        <td>Mason Porter</td>
-                        <td>Chile</td>
-                        <td>Gloucester</td>
-                        <td className="text-center">$78,615</td>
+                        <td>B-</td>
+                        <td>80 – 82.9%</td>
+                        <td>1200 – 1244</td>
+                        
                       </tr>
                       <tr>
-                        <td>Jon Porter</td>
-                        <td>Portugal</td>
-                        <td>Gloucester</td>
-                        <td className="text-center">$98,615</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="12">
-              <Card className="card-plain">
-                <CardHeader>
-                  <CardTitle tag="h4">Table on Plain Background</CardTitle>
-                  <p className="category">Here is a subtitle for this table</p>
-                </CardHeader>
-                <CardBody>
-                  <Table className="tablesorter" responsive>
-                    <thead className="text-primary">
-                      <tr>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th>City</th>
-                        <th className="text-center">Salary</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Dakota Rice</td>
-                        <td>Niger</td>
-                        <td>Oud-Turnhout</td>
-                        <td className="text-center">$36,738</td>
+                        <td>C+</td>
+                        <td>77 –  79.9%</td>
+                        <td>1155 – 1199</td>
+                        
                       </tr>
                       <tr>
-                        <td>Minerva Hooper</td>
-                        <td>Curaçao</td>
-                        <td>Sinaai-Waas</td>
-                        <td className="text-center">$23,789</td>
+                        <td>B</td>
+                        <td>83 – 86.9%</td>
+                        <td>1245 – 1304</td>
+        
                       </tr>
                       <tr>
-                        <td>Sage Rodriguez</td>
-                        <td>Netherlands</td>
-                        <td>Baileux</td>
-                        <td className="text-center">$56,142</td>
+                        <td>B-</td>
+                        <td>80 – 82.9%</td>
+                        <td>1200 – 1244</td>
+                        
                       </tr>
                       <tr>
-                        <td>Philip Chaney</td>
-                        <td>Korea, South</td>
-                        <td>Overland Park</td>
-                        <td className="text-center">$38,735</td>
+                        <td>C+</td>
+                        <td>77 –  79.9%</td>
+                        <td>1155 – 1199</td>
+                        
+                      </tr>
+                        <tr>
+                        <td>C</td>
+                        <td>73 – 76.9%</td>
+                        <td>1095 – 1154</td>
+        
                       </tr>
                       <tr>
-                        <td>Doris Greene</td>
-                        <td>Malawi</td>
-                        <td>Feldkirchen in Kärnten</td>
-                        <td className="text-center">$63,542</td>
+                        <td>C-</td>
+                        <td>70 – 72.9%</td>
+                        <td>1050 –1094</td>
+                        
                       </tr>
                       <tr>
-                        <td>Mason Porter</td>
-                        <td>Chile</td>
-                        <td>Gloucester</td>
-                        <td className="text-center">$78,615</td>
+                        <td>D</td>
+                        <td>60 – 69.9%</td>
+                        <td>900 – 1049</td>
+                        
                       </tr>
                       <tr>
-                        <td>Jon Porter</td>
-                        <td>Portugal</td>
-                        <td>Gloucester</td>
-                        <td className="text-center">$98,615</td>
+                        <td>F</td>
+                        <td>Below 60%</td>
+                        <td>Below 900</td>
+                        
                       </tr>
                     </tbody>
                   </Table>
@@ -169,3 +130,4 @@ class Tables extends React.Component {
 }
 
 export default Tables;
+
