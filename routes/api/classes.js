187a5fec468d7fc92f4ad510be71a0ee2a9685a7
Jake Battaglia <jbaked12@gmail.com>
2020-11-12 19:05:35 -0500

    working db's

--- routes/api/classes.js
+++ routes/api/classes.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const Course = require("../../models/Classes");
 
 router.post("/", (req, res) => {
-    console.log("POST CLASS:", req.body.className);
-    Course.create({className: req.body.className});
-
+    console.log("POST CLASS:", req.body);
+    Course.create({
+      className: req.body.className,
+      courseDescription: req.body.courseDescription,
+      location: req.body.location
+    
+    });
+    
    res.end(); 
  });
 
